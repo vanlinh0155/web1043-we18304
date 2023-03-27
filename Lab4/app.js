26022c746632df35939f71bbbf19adade7117117
Van Linh <linhnvpd08139@fpt.edu.vn>
2023-03-27 16:40:14 +0700

    Lab4

--- Lab4/app.js
+++ Lab4/app.js
@@ -1,3 +1,4 @@
+//Tao class STUDENT
 class Student {
     constructor(id, first_name, last_name, class_id, email, phone_number, dob) {
       this.id = id;
@@ -29,7 +30,7 @@ class Student {
       return age_day;
     }
   }
-  
+  //Tạo danh sách sinh viên
   const students = [
     new Student(1, 'Linh', 'Van', 1, 'vanlinh@gmail.com', '0901234567', '2004/05/01'),
     new Student(2, 'Hau', 'Nguyen', 1, 'haunv@gmail.com', '0901234568', '2004/03/02'),
@@ -37,12 +38,15 @@ class Student {
     new Student(4, 'Anh', 'Quan', 1, 'quan@gmail.com', '0901234570', '2004/10/04'),
     new Student(5, 'Bao', 'Ha', 1, 'habao@gmail.com', '0901234571', '2004/12/05')
   ];
-  
+  //Sắp xếp danh sách các sinh viên theo thứ tự từ thấp đến cao theo số tuổi 
   students.sort((a, b) => a.getAgeByDay() - b.getAgeByDay());
-  
+  //In ra danh sách sv theo thứ tự từ thấp đén cao theo số tuổi
   console.log('Danh sách sinh viên theo thứ tự số tuổi (tính theo ngày) từ thấp đến cao: ');
   students.forEach(student => {
     console.log(
       `ID: ${student.id}, Họ và tên: ${student.getFullName()}, Tuổi (theo năm): ${student.getAgeByYear()}, Tuổi (theo ngày): ${student.getAgeByDay()}`
     );
   });
+  var width = window.innerWidth;
+      var height = window.innerHeight;//bom để hiển thị độ dài w và h của browser
+      document.write(width+"px"+" "+height+"px");// hiển thị ta document 
