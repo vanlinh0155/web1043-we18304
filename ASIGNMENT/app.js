089cebfa05b99b65dd2519ce3d8ca3cb616de22d
Van Linh <linhnvpd08139@fpt.edu.vn>
2023-04-16 21:28:39 +0700

    ASSIGNMENT

--- ASIGNMENT/app.js
+++ ASIGNMENT/app.js
@@ -1,27 +1,28 @@
 
-//Menu Login
-const Loginform=document.querySelector('.Loginform')
-const MainButton=document.querySelector('.MainButton')
-const Registerform=document.querySelector('.Registerform')
-const Registerbtn=document.querySelector('.Registerbtn')
-MainButton.addEventListener('click',()=>{
-  Loginform.classList.add('Loginform-box')
-})
-Registerbtn.addEventListener('click',() =>{
-  Loginform.classList.remove('Loginform-box')
-  Registerform.classList.add('Registerform-box')
-  
-})
-//Media
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    document.body.style.backgroundColor = "#4EEE94";
-  } else {
-   document.body.style.backgroundColor = "#fff";
-  }
-}
+// Đặt thời gian kết thúc đếm ngược
+var countDownDate = new Date().getTime() + 12 * 60 * 60 * 1000; // 24 giờ từ thời điểm hiện tại
 
-var x = window.matchMedia("(max-width: 700px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
-//
+// Cập nhật thời gian đếm ngược mỗi giây
+var x = setInterval(function () {
+
+    // Lấy thời gian hiện tại
+    var now = new Date().getTime();
+
+    // Tính khoảng cách giữa thời gian hiện tại và thời gian kết thúc đếm ngược
+    var distance = countDownDate - now;
+
+    // Tính toán số giờ, phút và giây còn lại
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Hiển thị kết quả
+    document.getElementById("coutdown").innerHTML = hours + ": "
+        + minutes + ": " + seconds + " ";
+
+    // Nếu thời gian đếm ngược kết thúc, hiển thị thông báo
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("coutdown").innerHTML = "Hết giờ ";
+    }
+}, 1000);
